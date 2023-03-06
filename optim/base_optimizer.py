756dab72804f1d2dbb3a482904ee01029963d48e
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-03-06 17:38:55 -0600

    [Docs]: Add doctring for base optimizer class.

--- optim/base_optimizer.py
+++ optim/base_optimizer.py
@@ -1,6 +1,16 @@
 class BaseOptimizer(object):
+    """Base class for all optimizers.
+
+    Optimizers are responsible for updating the parameters of the model 
+    during training to minimize the loss function. 
+
+    Each optimizer should implement the `step` and `zero_grad` methods.
+    """
+
     def step(self):
+        """Performs a single optimization step."""
         raise NotImplementedError
 
     def zero_grad(self):
+        """Zeroes out the gradients of all parameters."""
         raise NotImplementedError
