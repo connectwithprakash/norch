bc81da6a6ef75fa645f7eac1bfbcca78c98efbaa
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-03-06 17:36:14 -0600

    [Docs]: Add doctring for linear class.

--- layer/linear.py
+++ layer/linear.py
@@ -5,7 +5,20 @@ from norch.layer import Module
 
 
 class Linear(Module):
+    """
+    A linear layer module that performs a linear transformation of input data.
+    """
+
     def __init__(self, in_features, out_features, bias=True, init_method='normal'):
+        """
+        Constructor for Linear layer.
+
+        Args:
+        - in_features: int, the number of input features
+        - out_features: int, the number of output features
+        - bias: bool, whether to include a bias term in the transformation
+        - init_method: str, the weight initialization method to use
+        """
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -17,16 +30,34 @@ class Linear(Module):
         self.init_weights()
 
     def init_weights(self):
+        """
+        Initializes the weight tensor and bias tensor (if bias=True) for the linear layer
+        """
         self.W = Tensor((self.in_features, self.out_features),
                         init_method=self.init_method)
         self.b = Tensor(self.out_features)
 
     def load_param(self, *params):
+        """
+        Loads weight and bias tensors into the linear layer.
+
+        Args:
+        - params: tuple, contains the weight and bias tensors
+        """
         self.W = params[0]
         if self.bias:
             self.b = params[1]
 
     def forward(self, input):
+        """
+        Forward pass through the linear layer.
+
+        Args:
+        - input: numpy array, the input data tensor
+
+        Returns:
+        - output: numpy array, the output data tensor after linear transformation
+        """
         self.input = input
         output = np.matmul(input, self.W)
         if self.bias:
@@ -34,6 +65,15 @@ class Linear(Module):
         return output
 
     def backward(self, gradwrtoutput):
+        """
+        Backward pass through the linear layer.
+
+        Args:
+        - gradwrtoutput: numpy array, the gradient of the loss with respect to the output of the layer
+
+        Returns:
+        - grad_input: numpy array, the gradient of the loss with respect to the input of the layer
+        """
         self.W.grad = np.matmul(self.input.T, gradwrtoutput)
         if self.bias:
             self.b.grad = np.sum(gradwrtoutput, axis=0)
@@ -41,11 +81,29 @@ class Linear(Module):
 
     @property
     def param(self):
+        """
+        Getter method for the weight and bias tensors.
+
+        Returns:
+        - params: list, the weight and bias tensors
+        """
         return [self.W, self.b]
 
     @property
     def grad(self):
+        """
+        Getter method for the gradients of the weight and bias tensors.
+
+        Returns:
+        - grads: list, the gradients of the weight and bias tensors
+        """
         return [self.W.grad, self.b.grad]
 
     def __repr__(self):
+        """
+        Returns a string representation of the linear layer.
+
+        Returns:
+        - string: str, a string representation of the linear layer
+        """
         return f"{self.__class__.__name__}(in_features={self.in_features}, out_features={self.out_features}, init_method=\"{self.init_method}\")"
